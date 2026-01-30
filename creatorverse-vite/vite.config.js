5f92dc03c77f719cae1d4d6dd8a1af683d231a7d
Bledar N <bledar.n@outlook.com>
2026-01-30 01:40:12 -0500

    ready to deploy

--- creatorverse-vite/vite.config.js
+++ creatorverse-vite/vite.config.js
@@ -1,9 +1,17 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   base: process.env.NODE_ENV === 'production'
+//     ? '/creatorverse-app/'
+//     : '/',
+//   plugins: [react()],
+// });
+
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { react } from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production'
-    ? '/creatorverse-app/'
-    : '/',
   plugins: [react()],
-});
+  base: '/creatorverse-app/',   // <-- IMPORTANT
+})
