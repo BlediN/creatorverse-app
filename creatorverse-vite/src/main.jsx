9e9429749972af19d5d835b31462ddf29c6d34cc
Bledar N <bledar.n@outlook.com>
2026-01-30 03:11:31 -0500

    fixd homepage route

--- creatorverse-vite/src/main.jsx
+++ creatorverse-vite/src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/creatorverse-app">
     <App />
   </BrowserRouter>
 );
